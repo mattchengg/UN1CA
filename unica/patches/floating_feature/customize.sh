f0f6c84e5dab7b1c09722c3c21838b0bdb8907f0
Salvo Giangreco <giangrecosalvo9@gmail.com>
2025-09-02 23:43:59 +0200

    unica: patches: floating_feature: overhaul
    - Automatic floating_feature.xml patching
    
    Change-Id: I3ec535bf7f1a3caf8712c5a0005761117b7a64b5

--- unica/patches/floating_feature/customize.sh
+++ unica/patches/floating_feature/customize.sh
@@ -1,34 +1,91 @@
-READ_AND_APPLY_CONFIGS()
+# [
+APPLY_TARGET_FEATURE()
 {
-    local CONFIG_FILE="$SRC_DIR/target/$TARGET_CODENAME/sff.sh"
+    local TARGET_FIRMWARE_PATH="$(cut -d "/" -f 1 -s <<< "$TARGET_FIRMWARE")_$(cut -d "/" -f 2 -s <<< "$TARGET_FIRMWARE")"
 
-    if [ -f "$CONFIG_FILE" ]; then
-        while read -r i; do
-            [[ "$i" = "#"* ]] && continue
-            [[ -z "$i" ]] && continue
+    local SOURCE_FILE="$WORK_DIR/system/system/etc/floating_feature.xml"
+    local TARGET_FILE="$FW_DIR/$TARGET_FIRMWARE_PATH/system/system/etc/floating_feature.xml"
 
-            if echo -n "$i" | grep -q "="; then
-                if [[ -z "$(echo -n "$i" | cut -d "=" -f 2)" ]]; then
-                    SET_FLOATING_FEATURE_CONFIG "$(echo -n "$i" | cut -d "=" -f 1)" --delete
-                else
-                    SET_FLOATING_FEATURE_CONFIG "$(echo -n "$i" | cut -d "=" -f 1)" "$(echo -n "$i" | cut -d "=" -f 2-)"
-                fi
-            else
-                echo "Malformed string in target/$TARGET_CODENAME/sff.sh: \"$i\""
-                return 1
+    local FEATURE
+    local SOURCE_VALUE
+    local TARGET_VALUE
+
+    # Step 1: iterate through work_dir floating_feature.xml
+    while IFS= read -r l; do
+        [[ "$l" == *"xml"* ]] && continue
+        [[ "$l" == *"SecFloatingFeatureSet"* ]] && continue
+        [ ! "$l" ] && continue
+
+        if [[ "$l" != "    <SEC_FLOATING_FEATURE_"*"</SEC_FLOATING_FEATURE_"*">" ]]; then
+            ABORT "Malformed string in ${SOURCE_FILE//$SRC_DIR\//}: \"$l\""
+        fi
+
+        FEATURE="$(awk -F '<|>' '{print $2}' <<< "$l")"
+
+        if ! grep -q -w "$FEATURE" "$TARGET_FILE"; then
+            SET_FLOATING_FEATURE_CONFIG "$FEATURE" --delete
+        else
+            SOURCE_VALUE="$(GET_FLOATING_FEATURE_CONFIG "$SOURCE_FILE" "$FEATURE")"
+            TARGET_VALUE="$(GET_FLOATING_FEATURE_CONFIG "$TARGET_FILE" "$FEATURE")"
+
+            if [[ "$SOURCE_VALUE" != "$TARGET_VALUE" ]]; then
+                SET_FLOATING_FEATURE_CONFIG "$FEATURE" "$TARGET_VALUE"
             fi
-        done < "$CONFIG_FILE"
-    fi
+        fi
+    done < "$SOURCE_FILE"
+
+    # Step 2: iterate through target floating_feature.xml
+    while IFS= read -r l; do
+        [[ "$l" == *"xml"* ]] && continue
+        [[ "$l" == *"SecFloatingFeatureSet"* ]] && continue
+        [ ! "$l" ] && continue
+
+        if [[ "$l" != "    <SEC_FLOATING_FEATURE_"*"</SEC_FLOATING_FEATURE_"*">" ]]; then
+            ABORT "Malformed string in ${TARGET_FILE//$SRC_DIR\//}: \"$l\""
+        fi
+
+        FEATURE="$(awk -F '<|>' '{print $2}' <<< "$l")"
+
+        if ! grep -q -w "$FEATURE" "$SOURCE_FILE"; then
+            SET_FLOATING_FEATURE_CONFIG "$FEATURE" "$(GET_FLOATING_FEATURE_CONFIG "$TARGET_FILE" "$FEATURE")"
+        fi
+    done < "$TARGET_FILE"
 }
 
-# Apply target floating feature
-READ_AND_APPLY_CONFIGS
+APPLY_CUSTOM_FEATURE()
+{
+    local FILE="$SRC_DIR/target/$TARGET_CODENAME/sff.sh"
 
+    if [ -f "$FILE" ]; then
+        while IFS= read -r l; do
+            [[ "$l" == "#"* ]] && continue
+            [ ! "$l" ] && continue
+
+            if [[ "$l" == "SEC_FLOATING_FEATURE_"*"="* ]]; then
+                if [ ! "$(cut -d "=" -f 2- <<< "$l")" ]; then
+                    SET_FLOATING_FEATURE_CONFIG "$(cut -d "=" -f 1 <<< "$l")" --delete
+                else
+                    SET_FLOATING_FEATURE_CONFIG "$(cut -d "=" -f 1 <<< "$l")" "$(cut -d "=" -f 2- <<< "$l")"
+                fi
+            else
+                ABORT "Malformed string in target/$TARGET_CODENAME/sff.sh: \"$l\""
+            fi
+        done < "$FILE"
+    fi
+}
+# ]
+
+APPLY_TARGET_FEATURE
+APPLY_CUSTOM_FEATURE
+
+# TODO move this somewhere else
 # Smart Tutor
 SET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_COMMON_CONFIG_SMARTTUTOR_PACKAGES_PATH" --delete
 
+# TODO move this somewhere else
 # Logging
 SET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_CONTEXTSERVICE_ENABLE_SURVEY_MODE" --delete
 
+# TODO move this somewhere else
 # BlockchainTZService
 SET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_FRAMEWORK_SUPPORT_BLOCKCHAIN_SERVICE" --delete
