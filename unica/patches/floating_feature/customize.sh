b538f2e0b82ede0e633a7c7475c2062db484ef06
Salvo Giangreco <giangrecosalvo9@gmail.com>
2025-09-04 23:22:58 +0200

    unica: patches: floating_feature: properly handle CAMERA_CONFIG_HIGH_RESOLUTION_MAX_CAPTURE feature
    Change-Id: Ie4b094b1a05797daf5b01281489bc56ff84859a8

--- unica/patches/floating_feature/customize.sh
+++ unica/patches/floating_feature/customize.sh
@@ -149,6 +149,9 @@ APPLY_TARGET_FEATURE()
 
         if [ ! "$TARGET_VALUE" ]; then
             TARGET_VALUE="$(cut -d "=" -f 2- < <(grep "$FEATURE" <<< "$FALLBACK"))"
+        elif [[ "$TARGET_VALUE"  == "-1" ]] && \
+                [[ "$FEATURE" == "SEC_FLOATING_FEATURE_CAMERA_CONFIG_HIGH_RESOLUTION_MAX_CAPTURE" ]]; then
+            TARGET_VALUE="0"
         fi
 
         if [ ! "$TARGET_VALUE" ]; then
