36d5a55d35f7de87e661a005f9bbeab111a03552
Salvo Giangreco <giangrecosalvo9@gmail.com>
2025-09-16 00:19:35 +0200

    unica: patches: rro: update
    - Adjust for newer OS versions
    - Update code to make use of the new utils
    
    Change-Id: I7f8ed9d60b11e33e64175400cd1d77e94c52f1e5

--- unica/patches/rro/customize.sh
+++ unica/patches/rro/customize.sh
@@ -1,9 +1,22 @@
-if [[ -d "$SRC_DIR/target/$TARGET_CODENAME/overlay" ]]; then
-    DECODE_APK "product" "overlay/framework-res__auto_generated_rro_product.apk"
-
-    echo "Applying stock overlay configs"
-    rm -rf "$APKTOOL_DIR/product/overlay/framework-res__auto_generated_rro_product.apk/res"
-    cp -a --preserve=all \
-        "$SRC_DIR/target/$TARGET_CODENAME/overlay" \
-        "$APKTOOL_DIR/product/overlay/framework-res__auto_generated_rro_product.apk/res"
+if [ ! -d "$SRC_DIR/target/$TARGET_CODENAME/overlay" ]; then
+    LOG "\033[0;33m! Nothing to do\033[0m"
+    return 0
 fi
+
+SOURCE_PRODUCT_NAME="$(GET_PROP "system" "ro.product.system.name")"
+TARGET_PRODUCT_NAME="$(GET_PROP "vendor" "ro.product.vendor.name")"
+
+DECODE_APK "product" "overlay/framework-res__${SOURCE_PRODUCT_NAME}__auto_generated_rro_product.apk"
+
+DELETE_FROM_WORK_DIR "product" "overlay/framework-res__${SOURCE_PRODUCT_NAME}__auto_generated_rro_product.apk"
+
+EVAL "mv -f \"$APKTOOL_DIR/product/overlay/framework-res__${SOURCE_PRODUCT_NAME}__auto_generated_rro_product.apk\" \"$APKTOOL_DIR/product/overlay/framework-res__${TARGET_PRODUCT_NAME}__auto_generated_rro_product.apk\""
+EVAL "sed -i \"s/${SOURCE_PRODUCT_NAME}/${TARGET_PRODUCT_NAME}/g\" \"$APKTOOL_DIR/product/overlay/framework-res__${TARGET_PRODUCT_NAME}__auto_generated_rro_product.apk/apktool.yml\""
+
+LOG "- Applying target product overlay"
+EVAL "rm -rf \"$APKTOOL_DIR/product/overlay/framework-res__${TARGET_PRODUCT_NAME}__auto_generated_rro_product.apk/res\""
+EVAL "cp -a \"$SRC_DIR/target/$TARGET_CODENAME/overlay\" \"$APKTOOL_DIR/product/overlay/framework-res__${TARGET_PRODUCT_NAME}__auto_generated_rro_product.apk/res\""
+
+SET_METADATA "product" "overlay/framework-res__${TARGET_PRODUCT_NAME}__auto_generated_rro_product.apk" 0 0 644 "u:object_r:system_file:s0"
+
+unset SOURCE_PRODUCT_NAME TARGET_PRODUCT_NAME
