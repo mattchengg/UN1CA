ef5878f0bf4d9d472b7c9e371c2034c38f6858e7
Salvo Giangreco <giangrecosalvo9@gmail.com>
2025-09-18 10:31:17 +0200

    unica: patches: legacy: add surfaceflinger props backport
    Change-Id: I10e2aecf49fb69230e9aefe9c8257aea83ae40fc

--- unica/patches/legacy/customize.sh
+++ unica/patches/legacy/customize.sh
@@ -3,6 +3,78 @@ if [[ "$SOURCE_API_LEVEL" == "$TARGET_API_LEVEL" ]]; then
     return 0
 fi
 
+# [
+BACKPORT_SF_PROPS()
+{
+    local FILE="$WORK_DIR/vendor/build.prop"
+    [ -f "$WORK_DIR/vendor/default.prop" ] && local FILE="$WORK_DIR/vendor/default.prop"
+
+    if [ ! -f "$FILE" ]; then
+        ABORT "File not found: ${FILE//$SRC_DIR\//}"
+    fi
+
+    local PROP
+    local VALUE
+
+    if [ "$TARGET_API_LEVEL" -lt "34" ]; then
+        PROP="ro.surface_flinger.enable_frame_rate_override"
+        VALUE="$(test "$TARGET_HFR_MODE" -gt "1" && echo "true" || echo "false")"
+
+        if [ ! "$(GET_PROP "vendor" "$PROP")" ]; then
+            LOG "- Adding \"$PROP\" prop with \"$VALUE\" in ${FILE//$WORK_DIR/}"
+            EVAL "sed -i \"/persist.sys.usb.config/i $PROP=$VALUE\" \"$FILE\""
+        fi
+    fi
+
+    if [ "$TARGET_API_LEVEL" -lt "35" ]; then
+        PROP="ro.surface_flinger.set_display_power_timer_ms"
+
+        if [ "$(GET_PROP "vendor" "$PROP")" ]; then
+            SET_PROP "vendor" "$PROP" --delete
+        fi
+
+        PROP="ro.surface_flinger.enable_frame_rate_override"
+        [ "$(GET_PROP "vendor" "ro.surface_flinger.set_idle_timer_ms")" ] && \
+            PROP="ro.surface_flinger.set_idle_timer_ms"
+        VALUE="$(GET_PROP "vendor" "ro.surface_flinger.use_content_detection_for_refresh_rate")"
+        [ ! "$VALUE" ] && \
+            VALUE="$(test "$TARGET_HFR_MODE" -gt "1" && echo "true" || echo "false")"
+
+        if [[ "$(sed -n "/$PROP/{x;p;d;}; x" "$FILE")" != *"use_content_detection_for_refresh_rate"* ]]; then
+            if [ ! "$(GET_PROP "vendor" "ro.surface_flinger.use_content_detection_for_refresh_rate")" ]; then
+                LOG "- Adding \"ro.surface_flinger.use_content_detection_for_refresh_rate\" prop with \"$VALUE\" in ${FILE//$WORK_DIR/}"
+            else
+                EVAL "sed -i \"/use_content_detection_for_refresh_rate/d\" \"$FILE\""
+            fi
+            EVAL "sed -i \"/$PROP/i ro.surface_flinger.use_content_detection_for_refresh_rate=$VALUE\" \"$FILE\""
+        fi
+
+        PROP="debug.sf.show_refresh_rate_overlay_render_rate"
+        VALUE="true"
+        if [ ! "$(GET_PROP "vendor" "$PROP")" ]; then
+            LOG "- Adding \"$PROP\" prop with \"$VALUE\" in ${FILE//$WORK_DIR/}"
+            EVAL "sed -i \"/ro.surface_flinger.use_content_detection_for_refresh_rate/i $PROP=$VALUE\" \"$FILE\""
+        fi
+
+        PROP="ro.surface_flinger.game_default_frame_rate_override"
+        VALUE="60"
+        if [ ! "$(GET_PROP "vendor" "$PROP")" ]; then
+            LOG "- Adding \"$PROP\" prop with \"$VALUE\" in ${FILE//$WORK_DIR/}"
+            EVAL "sed -i \"/debug.sf.show_refresh_rate_overlay_render_rate/a $PROP=$VALUE\" \"$FILE\""
+        fi
+    fi
+}
+# ]
+
+# Pre-API 34
+# - Add ro.surface_flinger.enable_frame_rate_override if missing
+#
+# Pre-API 35
+# - Place ro.surface_flinger.use_content_detection_for_refresh_rate correctly
+# - Add debug.sf.show_refresh_rate_overlay_render_rate if missing
+# - Add ro.surface_flinger.game_default_frame_rate_override if missing
+BACKPORT_SF_PROPS
+
 # Ensure config_num_physical_slots is configured (pre-API 36)
 # https://android.googlesource.com/platform/frameworks/opt/telephony/+/42e37234cee15c9f3fcfac0532110abfc8843b99%5E%21/#F0
 if [ "$TARGET_API_LEVEL" -lt "36" ]; then
@@ -24,3 +96,5 @@ if [ "$TARGET_API_LEVEL" -lt "35" ]; then
             "$(GET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_CAMERA_CONFIG_VENDOR_LIB_INFO"),image_codec.samsung.v1"
     fi
 fi
+
+unset -f BACKPORT_SF_PROPS
