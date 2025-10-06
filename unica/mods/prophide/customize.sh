890a193c95f763544ab735c75242ed386bae3cf1
utkustnr <utkuustuner@proton.me>
2025-10-06 19:16:40 +0200

    unica: mods: prophide: spoof AVB digest/size props
    Change-Id: I618ad385ace47b9bbd5e5d6fcc0d59a4a16bf877

--- unica/mods/prophide/customize.sh
+++ unica/mods/prophide/customize.sh
@@ -1,3 +1,9 @@
+VBIMG="$FW_DIR/$(cut -d "/" -f 1 -s <<< "$TARGET_FIRMWARE")_$(cut -d "/" -f 2 -s <<< "$TARGET_FIRMWARE")/avb/vbmeta.img"
+AVBINFO="$(avbtool info_image --image "$VBIMG")"
+AVBSIZE="$(awk '/Block:/ {sum+=$3} END {print sum}' <<< "$AVBINFO")"
+
+sed -i "s|VB_SIZE|$AVBSIZE|g" "$WORK_DIR/system/system/bin/prophide.sh"
+
 LINES="$(sed -n "/^(allow init init_exec\b/=" "$WORK_DIR/system/system/etc/selinux/plat_sepolicy.cil")"
 for l in $LINES; do
     sed -i "${l} s/)))/ execute_no_trans)))/" "$WORK_DIR/system/system/etc/selinux/plat_sepolicy.cil"
--- unica/mods/prophide/system/bin/prophide.sh
+++ unica/mods/prophide/system/bin/prophide.sh
@@ -1,10 +1,13 @@
 #!/system/bin/sh
+# https://android.googlesource.com/platform/external/avb/+/88b13e12a0ebe3c5195dbb5f48ba00ec896d1517
 
 rezetprop -n ro.boot.flash.locked "1"
 rezetprop -n ro.boot.vbmeta.avb_version "1.3"
 rezetprop -n ro.boot.vbmeta.device_state "locked"
+rezetprop -n ro.boot.vbmeta.digest "$(dd if=/dev/block/by-name/vbmeta bs=1 count=VB_SIZE 2> /dev/null | sha256sum -b)"
 rezetprop -n ro.boot.vbmeta.hash_alg "sha256"
 rezetprop -n ro.boot.vbmeta.invalidate_on_error "yes"
+rezetprop -n ro.boot.vbmeta.size "VB_SIZE"
 rezetprop -n ro.boot.warranty_bit "0"
 rezetprop -n ro.boot.verifiedbootstate "green"
 rezetprop -n ro.boot.veritymode "enforcing"
