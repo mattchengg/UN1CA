26866be4d8e4ec2588356b26d9802c7cbc985283
Salvo Giangreco <giangrecosalvo9@gmail.com>
2025-10-06 14:52:06 +0200

    unica: mods: blocklist: update for One UI 8.0
    Change-Id: I8d492cd52a6a1c921f70a068c4b5210feca548ed

--- unica/mods/blocklist/customize.sh
+++ unica/mods/blocklist/customize.sh
@@ -1,1 +1,6 @@
 DELETE_FROM_WORK_DIR "system" "system/etc/ldu_blocklist.xml"
+
+APPLY_PATCH "system" "system/framework/services.jar" \
+    "$SRC_DIR/unica/mods/blocklist/services.jar/0001-Allow-custom-PackageBlockListPolicy.patch"
+SMALI_PATCH "system" "system/framework/services.jar" \
+    "smali_classes2/com/samsung/android/server/pm/install/PackageBlockListPolicy\$1.smali" 'remove'
