ec0add1e087b7f7b5ff7ea23f87bb589a0a1be50
Salvo Giangreco <giangrecosalvo9@gmail.com>
2025-10-13 22:50:13 +0200

    unica: patches: dvfs: init
    Change-Id: I7e3f5421bf65917c7b243e4d5682c26a40bf8974

--- unica/patches/product_feature/customize.sh
+++ unica/patches/product_feature/customize.sh
@@ -213,44 +213,6 @@ else
     fi
 fi
 
-# SEC_PRODUCT_FEATURE_DVFSAPP_CONFIG_DVFS_POLICY_FILENAME
-if [[ "$SOURCE_DVFSAPP_CONFIG_DVFS_POLICY_FILENAME" != "$TARGET_DVFSAPP_CONFIG_DVFS_POLICY_FILENAME" ]]; then
-    SMALI_PATCH "system" "system/framework/ssrm.jar" \
-        "smali/com/android/server/ssrm/Feature.smali" "replace" \
-        "<clinit>()V" \
-        "$SOURCE_DVFSAPP_CONFIG_DVFS_POLICY_FILENAME" \
-        "$TARGET_DVFSAPP_CONFIG_DVFS_POLICY_FILENAME"
-    # com/sec/android/sdhms/performance/PerformanceFeature
-    SMALI_PATCH "system" "system/priv-app/SamsungDeviceHealthManagerService/SamsungDeviceHealthManagerService.apk" \
-        "smali/r1/c.smali" "replace" \
-        "<clinit>()V" \
-        "$SOURCE_DVFSAPP_CONFIG_DVFS_POLICY_FILENAME" \
-        "$TARGET_DVFSAPP_CONFIG_DVFS_POLICY_FILENAME"
-    # com/sec/android/sdhms/performance/settings/PerformanceProperties
-    SMALI_PATCH "system" "system/priv-app/SamsungDeviceHealthManagerService/SamsungDeviceHealthManagerService.apk" \
-        "smali/z1/e.smali" "replace" \
-        "<init>(Landroid/content/Context;)V" \
-        "$SOURCE_DVFSAPP_CONFIG_DVFS_POLICY_FILENAME" \
-        "$TARGET_DVFSAPP_CONFIG_DVFS_POLICY_FILENAME"
-fi
-
-# SEC_PRODUCT_FEATURE_DVFSAPP_CONFIG_SSRM_POLICY_FILENAME
-if [[ "$SOURCE_DVFSAPP_CONFIG_SSRM_POLICY_FILENAME" != "$TARGET_DVFSAPP_CONFIG_SSRM_POLICY_FILENAME" ]]; then
-    SET_FLOATING_FEATURE_CONFIG "SEC_FLOATING_FEATURE_SYSTEM_CONFIG_SIOP_POLICY_FILENAME" "$TARGET_DVFSAPP_CONFIG_SSRM_POLICY_FILENAME"
-
-    SMALI_PATCH "system" "system/framework/ssrm.jar" \
-        "smali/com/android/server/ssrm/Feature.smali" "replace" \
-        "<clinit>()V" \
-        "$SOURCE_DVFSAPP_CONFIG_SSRM_POLICY_FILENAME" \
-        "$TARGET_DVFSAPP_CONFIG_SSRM_POLICY_FILENAME"
-    # com/sec/android/sdhms/util/Feature
-    SMALI_PATCH "system" "system/priv-app/SamsungDeviceHealthManagerService/SamsungDeviceHealthManagerService.apk" \
-        "smali/U1/w.smali" "replace" \
-        "<clinit>()V" \
-        "$SOURCE_DVFSAPP_CONFIG_SSRM_POLICY_FILENAME" \
-        "$TARGET_DVFSAPP_CONFIG_SSRM_POLICY_FILENAME"
-fi
-
 # SEC_PRODUCT_FEATURE_FINGERPRINT_CONFIG_SENSOR
 if [[ "$SOURCE_FINGERPRINT_CONFIG_SENSOR" != "$TARGET_FINGERPRINT_CONFIG_SENSOR" ]]; then
     SMALI_PATCH "system" "system/framework/framework.jar" \
