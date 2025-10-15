1831f7eb3bd903462d6da640d2bf01b2a500a4e4
Salvo Giangreco <giangrecosalvo9@gmail.com>
2025-10-15 23:30:53 +0200

    unica: patches: uwb: update
    - Clean up code
    - Update blobs to One UI 8.0
    
    Change-Id: I1877b7c26dcbe74cce5f0454347157e202012aec

--- unica/patches/uwb/customize.sh
+++ unica/patches/uwb/customize.sh
@@ -1,42 +1,53 @@
-SOURCE_FIRMWARE_PATH="$FW_DIR/$(echo -n "$SOURCE_FIRMWARE" | sed 's./._.g' | rev | cut -d "_" -f2- | rev)"
-TARGET_FIRMWARE_PATH="$FW_DIR/$(echo -n "$TARGET_FIRMWARE" | sed 's./._.g' | rev | cut -d "_" -f2- | rev)"
+SOURCE_FIRMWARE_PATH="$(cut -d "/" -f 1 -s <<< "$SOURCE_FIRMWARE")_$(cut -d "/" -f 2 -s <<< "$SOURCE_FIRMWARE")"
+TARGET_FIRMWARE_PATH="$(cut -d "/" -f 1 -s <<< "$TARGET_FIRMWARE")_$(cut -d "/" -f 2 -s <<< "$TARGET_FIRMWARE")"
 
-if [ -f "$TARGET_FIRMWARE_PATH/system/system/etc/libuwb-cal.conf" ]; then
-    if ! grep -q "uwbcountrycode" "$WORK_DIR/product/etc/build.prop"; then
-        sed -i "/usb.config/a ro.boot.uwbcountrycode=ff" "$WORK_DIR/product/etc/build.prop"
-    fi
+SOURCE_HAS_UWB="$(test -f "$FW_DIR/$SOURCE_FIRMWARE_PATH/vendor/etc/permissions/android.hardware.uwb.xml" && echo "true" || echo "false")"
+TARGET_HAS_UWB="$(test -f "$FW_DIR/$TARGET_FIRMWARE_PATH/vendor/etc/permissions/android.hardware.uwb.xml" && echo "true" || echo "false")"
 
-    ADD_TO_WORK_DIR "$TARGET_FIRMWARE_PATH" "system" "system/etc/libuwb-cal.conf" 0 0 644 "u:object_r:system_file:s0"
-    ADD_TO_WORK_DIR "$TARGET_FIRMWARE_PATH" "system" "system/etc/pp_model.tflite" 0 0 644 "u:object_r:system_file:s0"
+if ! $SOURCE_HAS_UWB; then
+    if $TARGET_HAS_UWB; then
+        LOG "- Adding \"ro.boot.uwbcountrycode\" prop with \"ff\" in /product/etc/build.prop"
+        EVAL "sed -i \"/usb.config/a ro.boot.uwbcountrycode=ff\" \"$WORK_DIR/product/etc/build.prop\""
 
-    if [ ! -d "$SOURCE_FIRMWARE_PATH/system/system/app/UwbUci" ]; then
-        ADD_TO_WORK_DIR "dm3qxxx" "system" \
-            "system/app/UwbUci/UwbUci.apk" 0 0 644 "u:object_r:system_file:s0"
-        ADD_TO_WORK_DIR "dm3qxxx" "system" \
+        ADD_TO_WORK_DIR "b0qxxx" "product" \
+            "product/overlay/UwbRROverlay.apk" 0 0 644 "u:object_r:system_file:s0"
+        ADD_TO_WORK_DIR "b0qxxx" "system" \
+            "system/app/UwbTest/UwbTest.apk" 0 0 644 "u:object_r:system_file:s0"
+        ADD_TO_WORK_DIR "b0qxxx" "system" \
+            "system/etc/classpaths/bootclasspath.pb" 0 0 644 "u:object_r:system_file:s0"
+        ADD_TO_WORK_DIR "b0qxxx" "system" \
             "system/etc/init/init.system.uwb.rc" 0 0 644 "u:object_r:system_file:s0"
-        ADD_TO_WORK_DIR "dm3qxxx" "system" \
+        ADD_TO_WORK_DIR "b0qxxx" "system" \
             "system/etc/permissions/com.samsung.android.uwb_extras.xml" 0 0 644 "u:object_r:system_file:s0"
-        ADD_TO_WORK_DIR "dm3qxxx" "system" \
+        ADD_TO_WORK_DIR "b0qxxx" "system" \
             "system/etc/permissions/org.carconnectivity.android.digitalkey.timesync.xml" 0 0 644 "u:object_r:system_file:s0"
-        ADD_TO_WORK_DIR "dm3qxxx" "system" \
+        ADD_TO_WORK_DIR "b0qxxx" "system" \
             "system/etc/permissions/privapp-permissions-com.samsung.android.dcktimesync.xml" 0 0 644 "u:object_r:system_file:s0"
-        ADD_TO_WORK_DIR "dm3qxxx" "system" \
-            "system/etc/permissions/samsung.uwb.xml" 0 0 644 "u:object_r:system_file:s0"
-        ADD_TO_WORK_DIR "dm3qxxx" "system" \
+        ADD_TO_WORK_DIR "b0qxxx" "system" \
+            "system/etc/permissions/privapp-permissions-com.sec.android.app.uwbtest.xml" 0 0 644 "u:object_r:system_file:s0"
+        ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" \
+            "system/etc/libuwb-cal.conf" 0 0 644 "u:object_r:system_file:s0"
+        ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" \
+            "system/etc/pp_model.tflite" 0 0 644 "u:object_r:system_file:s0"
+        ADD_TO_WORK_DIR "b0qxxx" "system" \
             "system/framework/com.samsung.android.uwb_extras.jar" 0 0 644 "u:object_r:system_file:s0"
-        ADD_TO_WORK_DIR "dm3qxxx" "system" \
-            "system/framework/samsung.uwb.jar" 0 0 644 "u:object_r:system_file:s0"
-        ADD_TO_WORK_DIR "dm3qxxx" "system" \
+        ADD_TO_WORK_DIR "b0qxxx" "system" \
+            "system/framework/semuwb-service.jar" 0 0 644 "u:object_r:system_file:s0"
+        ADD_TO_WORK_DIR "b0qxxx" "system" \
             "system/lib/libtflite_uwb_jni.so" 0 0 644 "u:object_r:system_lib_file:s0"
-        ADD_TO_WORK_DIR "dm3qxxx" "system" \
-            "system/lib/libuwb_uci_jni_rust.so" 0 0 644 "u:object_r:system_lib_file:s0"
-        ADD_TO_WORK_DIR "dm3qxxx" "system" \
+        ADD_TO_WORK_DIR "b0qxxx" "system" \
             "system/lib64/libtflite_uwb_jni.so" 0 0 644 "u:object_r:system_lib_file:s0"
-        ADD_TO_WORK_DIR "dm3qxxx" "system" \
-            "system/lib64/libuwb_uci_jni_rust.so" 0 0 644 "u:object_r:system_lib_file:s0"
-        ADD_TO_WORK_DIR "dm3qxxx" "system_ext" \
+        ADD_TO_WORK_DIR "b0qxxx" "system_ext" \
             "framework/org.carconnectivity.android.digitalkey.timesync.jar" 0 0 644 "u:object_r:system_file:s0"
-        ADD_TO_WORK_DIR "dm3qxxx" "system_ext" \
+        ADD_TO_WORK_DIR "b0qxxx" "system_ext" \
             "priv-app/DckTimeSyncService/DckTimeSyncService.apk" 0 0 644 "u:object_r:system_file:s0"
+    else
+        LOG "\033[0;33m! Nothing to do\033[0m"
+    fi
+else
+    if ! $TARGET_HAS_UWB; then
+        ABORT "Missing patch for condition (SOURCE_HAS_UWB: [$SOURCE_HAS_UWB], TARGET_HAS_UWB: [$TARGET_HAS_UWB]). Aborting"
     fi
 fi
+
+unset SOURCE_FIRMWARE_PATH TARGET_FIRMWARE_PATH SOURCE_HAS_UWB TARGET_HAS_UWB
