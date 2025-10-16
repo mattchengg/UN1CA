3209efc82f53ca4bf8e8aaf4d6b3c3c5f142bbd1
Salvo Giangreco <giangrecosalvo9@gmail.com>
2025-10-16 21:46:30 +0200

    unica: patches: vndk: update VNDK 31 apex to One UI 8.0
    Change-Id: Iba0ef5f46fdeb32e178b05f1445c65a60c0e6991

--- unica/patches/vndk/customize.sh
+++ unica/patches/vndk/customize.sh
@@ -5,7 +5,7 @@ ADD_TARGET_VNDK_APEX() {
             ADD_TO_WORK_DIR "a73xqxx" "system_ext" "apex/com.android.vndk.v30.apex" 0 0 644 "u:object_r:system_file:s0"
             ;;
         "31")
-            ADD_TO_WORK_DIR "r0qxxx" "system_ext" "apex/com.android.vndk.v31.apex" 0 0 644 "u:object_r:system_file:s0"
+            ADD_TO_WORK_DIR "b0qxxx" "system_ext" "apex/com.android.vndk.v31.apex" 0 0 644 "u:object_r:system_file:s0"
             ;;
         "32")
             ADD_TO_WORK_DIR "b4qxxx" "system_ext" "apex/com.android.vndk.v32.apex" 0 0 644 "u:object_r:system_file:s0"
