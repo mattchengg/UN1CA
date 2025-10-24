eb9a807682b1de11e2ecd130a9cbfce7bb82c2b4
Khun Htetz Naing <khunhtetznaing@gmail.com>
2025-10-24 11:13:44 +0200

    unica: mods: monotype: init
    Change-Id: Ieee38e152f32e36ab184557a724d17df7deaf04b

--- unica/mods/settings/customize.sh
+++ unica/mods/settings/customize.sh
@@ -23,12 +23,6 @@ SMALI_PATCH "system" "system/priv-app/SecSettings/SecSettings.apk" \
     'isAvailable()Z' \
     'true'
 
-# Allow custom Monotype fonts
-SMALI_PATCH "system" "system/priv-app/SecSettings/SecSettings.apk" \
-    "smali_classes4/com/samsung/android/settings/display/SecDisplayUtils.smali" "return" \
-    'isInvalidFont(Landroid/content/Context;Ljava/lang/String;)Z' \
-    'false'
-
 LOG_STEP_IN "- Adding UN1CA Settings"
 
 # Dynamically patch SecSettings
