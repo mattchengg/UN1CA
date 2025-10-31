df3f0108ad8185c42ae115a83975746be09ce105
Salvo Giangreco <giangrecosalvo9@gmail.com>
2025-11-01 00:57:04 +0100

    unica: mods: setupwizard: enable navigation bar type settings step
    Change-Id: I8cd24a090bc6cd972b7a4e32f6691087bae77532

---
+++ unica/mods/setupwizard/customize.sh
@@ -0,0 +1,15 @@
+DECODE_APK "system" "system/priv-app/SecSetupWizard_Global/SecSetupWizard_Global.apk"
+
+LOG "- Enable navigation bar type settings step"
+SMALI_PATCH "system" "system/priv-app/SecSetupWizard_Global/SecSetupWizard_Global.apk" \
+    "smali/S2/f.smali" "replace" \
+    "d(Landroid/content/Context;Z)Ljava/util/ArrayList;" \
+    "navigationbar_setting" \
+    "this_string_does_not_exist" \
+    > /dev/null
+SMALI_PATCH "system" "system/priv-app/SecSetupWizard_Global/SecSetupWizard_Global.apk" \
+    "smali/com/sec/android/app/SecSetupWizard/SecSetupWizardActivity.smali" "replace" \
+    "f(Ljava/lang/String;)Z" \
+    "navigationbar_setting" \
+    "this_string_does_not_exist" \
+    > /dev/null
