e162620eea61786fb4698b7a5467cd926701efdf
Yagzie <97476482+Yagzie@users.noreply.github.com>
2025-11-01 16:30:56 +0100

    unica: mods: paradigm: add non-ACH 2025 ringtones
    Change-Id: I5b9b3382e2388ff037acfb0734642dfcf4c31a33

--- unica/mods/paradigm/customize.sh
+++ unica/mods/paradigm/customize.sh
@@ -1,11 +1,39 @@
+if $TARGET_AUDIO_SUPPORT_ACH_RINGTONE; then
+    SET_PROP "vendor" "ro.config.ringtone" "ACH_Galaxy_Bells.ogg"
+    SET_PROP "vendor" "ro.config.notification_sound" "ACH_Brightline.ogg"
+    SET_PROP "vendor" "ro.config.alarm_alert" "ACH_Morning_Xylophone.ogg"
+    SET_PROP "vendor" "ro.config.media_sound" "Media_preview_Over_the_horizon.ogg"
+    SET_PROP "vendor" "ro.config.ringtone_2" "ACH_Atomic_Bell.ogg"
+    SET_PROP "vendor" "ro.config.notification_sound_2" "ACH_Three_Star.ogg"
+else
+    SET_PROP "vendor" "ro.config.ringtone" "Galaxy_Bells.ogg"
+    SET_PROP "vendor" "ro.config.notification_sound" "Brightline.ogg"
+    SET_PROP "vendor" "ro.config.alarm_alert" "Morning_Xylophone.ogg"
+    SET_PROP "vendor" "ro.config.media_sound" "Media_preview_Over_the_horizon.ogg"
+    SET_PROP "vendor" "ro.config.ringtone_2" "Atomic_Bell.ogg"
+    SET_PROP "vendor" "ro.config.notification_sound_2" "Three_Star.ogg"
+fi
+
 DELETE_FROM_WORK_DIR "system" "system/media/audio/notifications"
 DELETE_FROM_WORK_DIR "system" "system/media/audio/ringtones"
 DELETE_FROM_WORK_DIR "system" "system/media/audio/ui"
+
 ADD_TO_WORK_DIR "pa1qxxx" "system" \
     "system/etc/permissions/privapp-permissions-com.samsung.android.sead.xml" 0 0 644 "u:object_r:system_file:s0"
-ADD_TO_WORK_DIR "pa1qxxx" "system" "system/etc/ringtones_count_list.txt" 0 0 644 "u:object_r:system_file:s0"
+if $TARGET_AUDIO_SUPPORT_ACH_RINGTONE; then
+    ADD_TO_WORK_DIR "pa1qxxx" "system" "system/etc/ringtones_count_list.txt" 0 0 644 "u:object_r:system_file:s0"
+else
+    ADD_TO_WORK_DIR "a56xnaxx" "system" "system/etc/ringtones_count_list.txt" 0 0 644 "u:object_r:system_file:s0"
+fi
 ADD_TO_WORK_DIR "pa1qxxx" "system" \
     "system/hidden/INTERNAL_SDCARD/Music/Samsung/Over_the_Horizon.m4a" 0 0 644 "u:object_r:system_file:s0"
-ADD_TO_WORK_DIR "pa1qxxx" "system" "system/media" 0 0 755 "u:object_r:system_file:s0"
+if $TARGET_AUDIO_SUPPORT_ACH_RINGTONE; then
+    ADD_TO_WORK_DIR "pa1qxxx" "system" "system/media/audio/notifications" 0 0 755 "u:object_r:system_file:s0"
+    ADD_TO_WORK_DIR "pa1qxxx" "system" "system/media/audio/ringtones" 0 0 755 "u:object_r:system_file:s0"
+else
+    ADD_TO_WORK_DIR "a56xnaxx" "system" "system/media/audio/notifications" 0 0 755 "u:object_r:system_file:s0"
+    ADD_TO_WORK_DIR "a56xnaxx" "system" "system/media/audio/ringtones" 0 0 755 "u:object_r:system_file:s0"
+fi
+ADD_TO_WORK_DIR "pa1qxxx" "system" "system/media/audio/ui" 0 0 755 "u:object_r:system_file:s0"
 ADD_TO_WORK_DIR "pa1qxxx" "system" \
     "system/priv-app/EnvironmentAdaptiveDisplay/EnvironmentAdaptiveDisplay.apk" 0 0 644 "u:object_r:system_file:s0"
