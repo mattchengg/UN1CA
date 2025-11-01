fb8da3e06bc785706eed962da8f385d0ed4f4a6b
utkustnr <utkuustuner@proton.me>
2025-11-01 16:52:39 +0100

    unica: mods: paradigm: clean up code
    Change-Id: I73571a1590d71be55140061418bca3668c68d8e1

--- unica/mods/paradigm/customize.sh
+++ unica/mods/paradigm/customize.sh
@@ -1,4 +1,13 @@
+# 2025 Audio Pack
+LOG_STEP_IN "- Adding 2025 Audio Pack"
+ADD_TO_WORK_DIR "pa1qxxx" "system" \
+    "system/hidden/INTERNAL_SDCARD/Music/Samsung/Over_the_Horizon.m4a" 0 0 644 "u:object_r:system_file:s0"
+DELETE_FROM_WORK_DIR "system" "system/media/audio/notifications"
+DELETE_FROM_WORK_DIR "system" "system/media/audio/ringtones"
 if $TARGET_AUDIO_SUPPORT_ACH_RINGTONE; then
+    ADD_TO_WORK_DIR "pa1qxxx" "system" "system/etc/ringtones_count_list.txt" 0 0 644 "u:object_r:system_file:s0"
+    ADD_TO_WORK_DIR "pa1qxxx" "system" "system/media/audio/notifications" 0 0 755 "u:object_r:system_file:s0"
+    ADD_TO_WORK_DIR "pa1qxxx" "system" "system/media/audio/ringtones" 0 0 755 "u:object_r:system_file:s0"
     SET_PROP "vendor" "ro.config.ringtone" "ACH_Galaxy_Bells.ogg"
     SET_PROP "vendor" "ro.config.notification_sound" "ACH_Brightline.ogg"
     SET_PROP "vendor" "ro.config.alarm_alert" "ACH_Morning_Xylophone.ogg"
@@ -6,6 +15,9 @@ if $TARGET_AUDIO_SUPPORT_ACH_RINGTONE; then
     SET_PROP "vendor" "ro.config.ringtone_2" "ACH_Atomic_Bell.ogg"
     SET_PROP "vendor" "ro.config.notification_sound_2" "ACH_Three_Star.ogg"
 else
+    ADD_TO_WORK_DIR "a56xnaxx" "system" "system/etc/ringtones_count_list.txt" 0 0 644 "u:object_r:system_file:s0"
+    ADD_TO_WORK_DIR "a56xnaxx" "system" "system/media/audio/notifications" 0 0 755 "u:object_r:system_file:s0"
+    ADD_TO_WORK_DIR "a56xnaxx" "system" "system/media/audio/ringtones" 0 0 755 "u:object_r:system_file:s0"
     SET_PROP "vendor" "ro.config.ringtone" "Galaxy_Bells.ogg"
     SET_PROP "vendor" "ro.config.notification_sound" "Brightline.ogg"
     SET_PROP "vendor" "ro.config.alarm_alert" "Morning_Xylophone.ogg"
@@ -13,27 +25,20 @@ else
     SET_PROP "vendor" "ro.config.ringtone_2" "Atomic_Bell.ogg"
     SET_PROP "vendor" "ro.config.notification_sound_2" "Three_Star.ogg"
 fi
+ADD_TO_WORK_DIR "pa1qxxx" "system" \
+    "system/media/audio/ui/Media_preview_Over_the_horizon.ogg" 0 0 644 "u:object_r:system_file:s0"
+LOG_STEP_OUT
 
-DELETE_FROM_WORK_DIR "system" "system/media/audio/notifications"
-DELETE_FROM_WORK_DIR "system" "system/media/audio/ringtones"
-DELETE_FROM_WORK_DIR "system" "system/media/audio/ui"
-
+# Adaptive colour tone
 ADD_TO_WORK_DIR "pa1qxxx" "system" \
     "system/etc/permissions/privapp-permissions-com.samsung.android.sead.xml" 0 0 644 "u:object_r:system_file:s0"
-if $TARGET_AUDIO_SUPPORT_ACH_RINGTONE; then
-    ADD_TO_WORK_DIR "pa1qxxx" "system" "system/etc/ringtones_count_list.txt" 0 0 644 "u:object_r:system_file:s0"
-else
-    ADD_TO_WORK_DIR "a56xnaxx" "system" "system/etc/ringtones_count_list.txt" 0 0 644 "u:object_r:system_file:s0"
-fi
-ADD_TO_WORK_DIR "pa1qxxx" "system" \
-    "system/hidden/INTERNAL_SDCARD/Music/Samsung/Over_the_Horizon.m4a" 0 0 644 "u:object_r:system_file:s0"
-if $TARGET_AUDIO_SUPPORT_ACH_RINGTONE; then
-    ADD_TO_WORK_DIR "pa1qxxx" "system" "system/media/audio/notifications" 0 0 755 "u:object_r:system_file:s0"
-    ADD_TO_WORK_DIR "pa1qxxx" "system" "system/media/audio/ringtones" 0 0 755 "u:object_r:system_file:s0"
-else
-    ADD_TO_WORK_DIR "a56xnaxx" "system" "system/media/audio/notifications" 0 0 755 "u:object_r:system_file:s0"
-    ADD_TO_WORK_DIR "a56xnaxx" "system" "system/media/audio/ringtones" 0 0 755 "u:object_r:system_file:s0"
-fi
-ADD_TO_WORK_DIR "pa1qxxx" "system" "system/media/audio/ui" 0 0 755 "u:object_r:system_file:s0"
 ADD_TO_WORK_DIR "pa1qxxx" "system" \
     "system/priv-app/EnvironmentAdaptiveDisplay/EnvironmentAdaptiveDisplay.apk" 0 0 644 "u:object_r:system_file:s0"
+APPLY_PATCH "system" "system/framework/services.jar" \
+    "$MODPATH/ead/services.jar/0001-Add-Adaptive-color-tone-feature.patch"
+APPLY_PATCH "system" "system/priv-app/SecSettings/SecSettings.apk" \
+    "$MODPATH/ead/SecSettings.apk/0001-Add-Adaptive-color-tone-feature.patch"
+APPLY_PATCH "system" "system/priv-app/SettingsProvider/SettingsProvider.apk" \
+    "$MODPATH/ead/SettingsProvider.apk/0001-Add-Adaptive-color-tone-feature.patch"
+APPLY_PATCH "system_ext" "priv-app/SystemUI/SystemUI.apk" \
+    "$MODPATH/ead/SystemUI.apk/0001-Add-Adaptive-color-tone-toggle.patch"
