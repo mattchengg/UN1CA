f2677d41a47d5df737a72b77d3db2975bbbff46a
Salvo Giangreco <giangrecosalvo9@gmail.com>
2025-11-03 00:10:19 +0100

    unica: mods: wallpaper: update to S25 infinity wallpapers
    Change-Id: I87f451e180c3849a64817091f4e50c81cb8d2892

--- unica/mods/wallpaper/customize.sh
+++ unica/mods/wallpaper/customize.sh
@@ -1,36 +1,85 @@
 # [
-RESIZE_APK()
+COMPRESS_WEBP()
 {
-    local WEBP_RES="2400"
-    local MP4_RES="1080:-1"
+    local FILE="$1"
+    local FILE_PATH
+    local FILE_NAME
+    local RES="2400"
+    local CMD
+
+    FILE_PATH="$(dirname "$FILE")"
+    FILE_NAME="$(basename "$FILE")"
 
     if $TARGET_COMMON_SUPPORT_DYN_RESOLUTION_CONTROL; then
-        if [[ "$TARGET_PRODUCT_SHIPPING_API_LEVEL" -le 30 ]]; then
-            WEBP_RES="3200"
+        if [ "$TARGET_PRODUCT_SHIPPING_API_LEVEL" -gt "30" ] && \
+                [ "$TARGET_PRODUCT_SHIPPING_API_LEVEL" -lt "34" ]; then
+            RES="3088"
         else
-            WEBP_RES="3088"
+            RES="3120"
         fi
-        MP4_RES="1440:-1"
     fi
 
-    echo "Resize wallpaper-res.apk"
-    DECODE_APK "system" "system/priv-app/wallpaper-res/wallpaper-res.apk"
-    for f in "$APKTOOL_DIR/system/priv-app/wallpaper-res/wallpaper-res.apk/res/drawable-nodpi/Wallpaper_"*.webp
-    do
-        cwebp -q 100 -resize $WEBP_RES $WEBP_RES "$f" -o "$(dirname "$f")/temp.webp" &> /dev/null
-        mv -f "$(dirname "$f")/temp.webp" "$f"
-    done
-    for f in "$APKTOOL_DIR/system/priv-app/wallpaper-res/wallpaper-res.apk/res/raw/"*.mp4
-    do
-        ffmpeg -i "$f" -c:v libx265 -tag:v hvc1 -c:a copy \
-            -crf 18 -movflags use_metadata_tags -map_metadata 0 \
-            -vf "scale=$MP4_RES,setsar=1:1" -video_track_timescale 60000 \
-            "$(dirname "$f")/temp.mp4" &> /dev/null
-        mv -f "$(dirname "$f")/temp.mp4" "$f"
-    done
+    LOG "- Compressing $FILE_NAME"
+
+    CMD="cwebp"
+    CMD+=" -q 100"
+    CMD+=" -resize $RES $RES"
+    CMD+=" \"$FILE_PATH/$FILE_NAME\""
+    CMD+=" -o \"$FILE_PATH/temp.webp\""
+
+    EVAL "$CMD" || return 1
+    EVAL "mv -f \"$FILE_PATH/temp.webp\" \"$FILE_PATH/$FILE_NAME\"" || return 1
+}
+
+ENCODE_MP4()
+{
+    local FILE="$1"
+    local FILE_PATH
+    local FILE_NAME
+    local RES="-1:2400"
+    local CMD
+
+    FILE_PATH="$(dirname "$FILE")"
+    FILE_NAME="$(basename "$FILE")"
+
+    if $TARGET_COMMON_SUPPORT_DYN_RESOLUTION_CONTROL; then
+        RES="1440:-1"
+    fi
+
+    LOG "- Encoding $FILE_NAME"
+
+    CMD="ffmpeg"
+    CMD+=" -i \"$FILE_PATH/$FILE_NAME\""
+    CMD+=" -c:v libx264 -c:a copy"
+    CMD+=" -pix_fmt yuv420p -crf 18 -g 1"
+    CMD+=" -preset veryslow -tune zerolatency"
+    CMD+=" -movflags use_metadata_tags -map_metadata 0"
+    CMD+=" -vf \"fps=60,scale=$RES,setsar=1:1\""
+    CMD+=" -video_track_timescale 360000 -movie_timescale 90000"
+    CMD+=" \"$FILE_PATH/temp.mp4\""
+
+    EVAL "$CMD" || return 1
+    EVAL "mv -f \"$FILE_PATH/temp.mp4\" \"$FILE_PATH/$FILE_NAME\"" || return 1
 }
 # ]
 
-ADD_TO_WORK_DIR "e1qzcx" "system" \
+ADD_TO_WORK_DIR "pa1qxxx" "system" \
     "system/priv-app/wallpaper-res/wallpaper-res.apk" 0 0 644 "u:object_r:system_file:s0"
-RESIZE_APK
+DECODE_APK "system" "system/priv-app/wallpaper-res/wallpaper-res.apk"
+for f in "$APKTOOL_DIR/system/priv-app/wallpaper-res/wallpaper-res.apk/res/drawable-nodpi/dex_wallpaper_"*.webp; do
+    COMPRESS_WEBP "$f"
+done
+for f in "$APKTOOL_DIR/system/priv-app/wallpaper-res/wallpaper-res.apk/res/drawable-nodpi/wallpaper_"*.webp; do
+    COMPRESS_WEBP "$f"
+done
+for f in "$APKTOOL_DIR/system/priv-app/wallpaper-res/wallpaper-res.apk/res/raw/video_"*.mp4; do
+    ENCODE_MP4 "$f"
+done
+APPLY_PATCH "system" "system/priv-app/SpriteWallpaper/SpriteWallpaper.apk" \
+    "$MODPATH/SpriteWallpaper.apk/0001-Force-Paradigm-wallpapers-motion-animator.patch"
+APPLY_PATCH "system" "system/priv-app/SpriteWallpaper/SpriteWallpaper.apk" \
+    "$MODPATH/SpriteWallpaper.apk/0002-Adjust-motion-animator-for-60fps-video-files.patch"
+APPLY_PATCH "system" "system/priv-app/wallpaper-res/wallpaper-res.apk" \
+    "$MODPATH/wallpaper-res.apk/0001-Adjust-metadata-for-60fps-video-files.patch"
+
+unset -f ENCODE_MP4 COMPRESS_WEBP
