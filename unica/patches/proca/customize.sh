491a7beab4ea1cb78c322abac9e7f1a49a5b4719
Salvo Giangreco <giangrecosalvo9@gmail.com>
2025-11-07 00:25:19 +0100

    unica: patches: proca: overhaul
    - Code clean up
    - Support compressed kernel images
    
    Change-Id: I957af07dcafa02e2caf50479ebf2343b4daeb5fe

--- unica/patches/proca/customize.sh
+++ unica/patches/proca/customize.sh
@@ -1,40 +1,58 @@
+[ ! -f "$WORK_DIR/kernel/boot.img" ] && ABORT "File not found: ${WORK_DIR//$SRC_DIR\//}/kernel/boot.img"
+
+LOG "- Extracting boot.img"
+
+[ -d "$TMP_DIR" ] && EVAL "rm -rf \"$TMP_DIR\""
+EVAL "mkdir -p \"$TMP_DIR\""
+EVAL "cp -a \"$WORK_DIR/kernel/boot.img\" \"$TMP_DIR/boot.img\""
+
+MKBOOTIMG_ARGS="$(unpack_bootimg --boot_img "$TMP_DIR/boot.img" --out "$TMP_DIR/out" --format mkbootimg 2>&1)"
+
+[ ! -f "$TMP_DIR/out/kernel" ] && ABORT "Failed to extract boot.img\n\n$MKBOOTIMG_ARGS"
+
+GZ_COMPRESSED=false
+[[ "$(READ_BYTES_AT "$TMP_DIR/out/kernel" "0" "2")" == "8b1f" ]] && GZ_COMPRESSED=true
+if $GZ_COMPRESSED; then
+    LOG "- Decompressing kernel image"
+    EVAL "cat \"$TMP_DIR/out/kernel\" | gzip -d > \"$TMP_DIR/out/tmp\" && mv -f \"$TMP_DIR/out/tmp\" \"$TMP_DIR/out/kernel\""
+fi
+
+if [[ "$(LC_ALL=C file -b "$TMP_DIR/out/kernel")" != "Linux kernel ARM64"* ]]; then
+    ABORT "Kernel image not valid\n\n$(LC_ALL=C file -b "$TMP_DIR/out/kernel")"
+fi
+
 PATCHED=false
 
-if [ ! -f "$WORK_DIR/kernel/boot.img" ]; then
-    echo "File not found: $WORK_DIR/kernel/boot.img"
-    exit 1
+PROCA_CONFIG_ADDR="$(READ_BYTES_AT "$TMP_DIR/out/kernel" "40" "4")"
+if [[ "$PROCA_CONFIG_ADDR" != "00000000" ]] && [[ "$PROCA_CONFIG_ADDR" != "ecefecef" ]]; then
+    LOG "- Patching PROCA offset in kernel image header"
+    EVAL "printf \"\\xef\\xec\\xef\\xec\" | dd of=\"$TMP_DIR/out/kernel\" bs=1 seek=40 count=4 conv=notrunc"
+    PATCHED=true
 fi
 
-[ -d "$TMP_DIR" ] && rm -rf "$TMP_DIR"
-mkdir -p "$TMP_DIR"
-
-cp -a --preserve=all "$WORK_DIR/kernel/boot.img" "$TMP_DIR/boot.img"
-MKBOOTIMG_ARGS="$(unpack_bootimg --boot_img "$TMP_DIR/boot.img" --out "$TMP_DIR/out" --format mkbootimg)"
-
-if [ ! -f "$TMP_DIR/out/kernel" ]; then
-    echo -e "Failed to extract boot.img:\n$MKBOOTIMG_ARGS"
-    exit 1
+if grep -q -P -w "proca_config\x00" "$TMP_DIR/out/kernel"; then
+    HEX_PATCH "$TMP_DIR/out/kernel" "70726f63615f636f6e66696700" "6675636b5f755f73616d6d7900"
+    PATCHED=true
 fi
 
-if [[ "$(file -b "$TMP_DIR/out/kernel")" == "Linux kernel ARM64"* ]]; then
-    PROCA_CONFIG_ADDR="$(xxd -p -l "4" --skip "40" "$TMP_DIR/out/kernel")"
-    if [[ "$PROCA_CONFIG_ADDR" != "00000000" ]] || [[ "$PROCA_CONFIG_ADDR" != "efecefec" ]]; then
-        printf "\xef\xec\xef\xec" | dd of="$TMP_DIR/out/kernel" bs=1 seek=40 count=4 conv=notrunc &> /dev/null
-        PATCHED=true
-    fi
-
-    if grep -q -w "proca_config" "$TMP_DIR/out/kernel"; then
-        sed -i "s/proca_config\x00/fuck_u_sammy\x00/g" "$TMP_DIR/out/kernel"
-        PATCHED=true
-    fi
-
-    if $PATCHED; then
-        eval "mkbootimg $MKBOOTIMG_ARGS -o $TMP_DIR/new-boot.img"
-        echo -n "SEANDROIDENFORCE" >> "$TMP_DIR/new-boot.img"
-        mv -f "$TMP_DIR/new-boot.img" "$WORK_DIR/kernel/boot.img"
-    fi
-else
-    echo -e "Ignoring kernel image ($(file -b "$TMP_DIR/out/kernel"))"
+if ! $PATCHED; then
+    LOG "\033[0;33m! Nothing to do\033[0m"
+    EVAL "rm -rf \"$TMP_DIR\""
+    unset MKBOOTIMG_ARGS GZ_COMPRESSED PATCHED PROCA_CONFIG_ADDR
+    return 0
 fi
 
-rm -rf "$TMP_DIR"
+if $GZ_COMPRESSED; then
+    LOG "- Compressing kernel image"
+    EVAL "cat \"$TMP_DIR/out/kernel\" | gzip -n -f -9 > \"$TMP_DIR/out/tmp\" && mv -f \"$TMP_DIR/out/tmp\" \"$TMP_DIR/out/kernel\""
+fi
+
+LOG "- Repacking boot.img"
+
+EVAL "mkbootimg $MKBOOTIMG_ARGS -o \"$TMP_DIR/new-boot.img\""
+echo -n "SEANDROIDENFORCE" >> "$TMP_DIR/new-boot.img"
+EVAL "mv -f \"$TMP_DIR/new-boot.img\" \"$WORK_DIR/kernel/boot.img\""
+
+EVAL "rm -rf \"$TMP_DIR\""
+
+unset MKBOOTIMG_ARGS GZ_COMPRESSED PATCHED PROCA_CONFIG_ADDR
