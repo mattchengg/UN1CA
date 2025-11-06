mattchengg/UN1CA

commit 016098bcb87a368d958de281848e170ccde19fa0

scripts: gen_config_file: overhaul
- Rename build flags to match Samsung/AOSP build system
- Require SEAMLESS_BRT/LUX when VRR is enabled
- Dynamically generate super partition group size flag to match AOSP build system

Change-Id: I26fbc3ea299b2d31c222cbef8217292c80730402

scripts/internal/build_flashable_zip.sh
@@ -65,7 +65,7 @@ BUILD_SUPER_EMPTY()
     # https://android.googlesource.com/platform/build/+/refs/tags/android-15.0.0_r1/tools/releasetools/build_super_image.py#85
     CMD+=" --metadata-slots \"2\""
     CMD+=" --device \"super:$TARGET_SUPER_PARTITION_SIZE\""
-    CMD+=" --group \"$TARGET_SUPER_GROUP_NAME:$TARGET_SUPER_GROUP_SIZE\""
+    CMD+=" --group \"$TARGET_SUPER_GROUP_NAME:$(GET_SUPER_GROUP_SIZE)\""
     if [ -f "$TMP_DIR/system.img" ]; then
         CMD+=" --partition \"system:readonly:0:$TARGET_SUPER_GROUP_NAME\""
     fi
@@ -136,8 +136,8 @@ GENERATE_OP_LIST()
     {
         echo "# Remove all existing dynamic partitions and groups before applying full OTA"
         echo "remove_all_groups"
-        echo "# Add group $TARGET_SUPER_GROUP_NAME with maximum size $TARGET_SUPER_GROUP_SIZE"
-        echo "add_group $TARGET_SUPER_GROUP_NAME $TARGET_SUPER_GROUP_SIZE"
+        echo "# Add group $TARGET_SUPER_GROUP_NAME with maximum size $(GET_SUPER_GROUP_SIZE)"
+        echo "add_group $TARGET_SUPER_GROUP_NAME $(GET_SUPER_GROUP_SIZE)"
         $HAS_SYSTEM && echo "# Add partition system to group $TARGET_SUPER_GROUP_NAME"
         $HAS_SYSTEM && echo "add system $TARGET_SUPER_GROUP_NAME"
         $HAS_VENDOR && echo "# Add partition vendor to group $TARGET_SUPER_GROUP_NAME"
@@ -204,8 +204,8 @@ GENERATE_OP_LIST()
         fi
     } > "$OP_LIST_FILE"
 
-    if [[ "$OCCUPIED_SPACE" -gt "$TARGET_SUPER_GROUP_SIZE" ]]; then
-        LOGE "OS size ($OCCUPIED_SPACE) is bigger than the target group size ($TARGET_SUPER_GROUP_SIZE)"
+    if [[ "$OCCUPIED_SPACE" -gt "$(GET_SUPER_GROUP_SIZE)" ]]; then
+        LOGE "OS size ($OCCUPIED_SPACE) is bigger than the target group size ($(GET_SUPER_GROUP_SIZE))"
         exit 1
     fi
 }
@@ -401,25 +401,25 @@ GENERATE_UPDATER_SCRIPT()
         if $HAS_DTBO; then
             echo    'ui_print("Full Patching dtbo.img img...");'
             echo -n 'package_extract_file("dtbo.img", "'
-            echo -n "$TARGET_BOOT_DEVICE_PATH"
+            echo -n "$TARGET_OS_BOOT_DEVICE_PATH"
             echo    '/dtbo");'
         fi
         if $HAS_INIT_BOOT; then
             echo    'ui_print("Full Patching init_boot.img img...");'
             echo -n 'package_extract_file("init_boot.img", "'
-            echo -n "$TARGET_BOOT_DEVICE_PATH"
+            echo -n "$TARGET_OS_BOOT_DEVICE_PATH"
             echo    '/init_boot");'
         fi
         if $HAS_VENDOR_BOOT; then
             echo    'ui_print("Full Patching vendor_boot.img img...");'
             echo -n 'package_extract_file("vendor_boot.img", "'
-            echo -n "$TARGET_BOOT_DEVICE_PATH"
+            echo -n "$TARGET_OS_BOOT_DEVICE_PATH"
             echo    '/vendor_boot");'
         fi
         if $HAS_BOOT; then
             echo    'ui_print("Installing boot image...");'
             echo -n 'package_extract_file("boot.img", "'
-            echo -n "$TARGET_BOOT_DEVICE_PATH"
+            echo -n "$TARGET_OS_BOOT_DEVICE_PATH"
             echo    '/boot");'
         fi
 
@@ -431,6 +431,16 @@ GENERATE_UPDATER_SCRIPT()
         echo    'ui_print("************************************************");'
         echo    'ui_print(" ");'
     } > "$SCRIPT_FILE"
+}
+
+GET_SUPER_GROUP_SIZE()
+{
+    local GROUP_NAME="$TARGET_SUPER_GROUP_NAME"
+    GROUP_NAME="$(tr "[:lower:]" "[:upper:]" <<< "$TARGET_SUPER_GROUP_NAME")"
+
+    _CHECK_NON_EMPTY_PARAM "TARGET_${GROUP_NAME}_SIZE" "${!TARGET_${GROUP_NAME}_SIZE}" || exit 1
+
+    echo "${!TARGET_${GROUP_NAME}_SIZE}"
 }
 
 PRINT_HEADER()
@@ -523,7 +533,7 @@ while IFS= read -r f; do
     PARTITION=$(basename "$f")
     IS_VALID_PARTITION_NAME "$PARTITION" || continue
 
-    "$SRC_DIR/scripts/build_fs_image.sh" "$TARGET_OS_FILE_SYSTEM" \
+    "$SRC_DIR/scripts/build_fs_image.sh" "$TARGET_OS_FILE_SYSTEM_TYPE" \
         -o "$TMP_DIR/$PARTITION.img" -m -S \
         "$WORK_DIR/$PARTITION" "$WORK_DIR/configs/file_context-$PARTITION" "$WORK_DIR/configs/fs_config-$PARTITION" || exit 1
 done < <(find "$WORK_DIR" -maxdepth 1 -type d)

scripts/apktool.sh
@@ -157,7 +157,7 @@ PREPARE_SCRIPT()
     local FILE_PATH="$WORK_DIR"
     case "$PARTITION" in
         "system_ext")
-            if $TARGET_HAS_SYSTEM_EXT; then
+            if $TARGET_OS_BUILD_SYSTEM_EXT_PARTITION; then
                 FILE_PATH+="/system_ext"
             else
                 FILE_PATH+="/system/system/system_ext"

scripts/internal/apply_modules.sh
@@ -32,8 +32,8 @@ APPLY_MODULE()
         return 1
     fi
 
-    if [ -d "$MODPATH/$TARGET_SINGLE_SYSTEM_IMAGE" ]; then
-        MODPATH="$MODPATH/$TARGET_SINGLE_SYSTEM_IMAGE"
+    if [ -d "$MODPATH/$TARGET_OS_SINGLE_SYSTEM_IMAGE" ]; then
+        MODPATH="$MODPATH/$TARGET_OS_SINGLE_SYSTEM_IMAGE"
     fi
 
     if [ ! -f "$MODPATH/module.prop" ]; then

scripts/internal/create_work_dir.sh
@@ -39,7 +39,7 @@ COPY_SOURCE_FIRMWARE()
     done
 
     if [ -d "$FW_DIR/$SOURCE_FIRMWARE_PATH/system_ext" ]; then
-        if $TARGET_HAS_SYSTEM_EXT; then
+        if $TARGET_OS_BUILD_SYSTEM_EXT_PARTITION; then
             LOG_STEP_IN "- Copying /system_ext from source firmware"
 
             [ -L "$WORK_DIR/system/system_ext" ] && rm -f "$WORK_DIR/system/system_ext"
@@ -76,7 +76,7 @@ COPY_SOURCE_FIRMWARE()
             LOG_STEP_OUT
         fi
     elif [ -d "$FW_DIR/$SOURCE_FIRMWARE_PATH/system/system/system_ext" ]; then
-        if $TARGET_HAS_SYSTEM_EXT; then
+        if $TARGET_OS_BUILD_SYSTEM_EXT_PARTITION; then
             LOG_STEP_IN "- Copying /system_ext from source firmware"
 
             [ -L "$WORK_DIR/system/system_ext" ] && rm -f "$WORK_DIR/system/system_ext"

scripts/print_modules_info.sh
@@ -33,8 +33,8 @@ PRINT_MODULE_INFO()
         exit 1
     fi
 
-    if [ -d "$MODPATH/$TARGET_SINGLE_SYSTEM_IMAGE" ]; then
-        MODPATH="$MODPATH/$TARGET_SINGLE_SYSTEM_IMAGE"
+    if [ -d "$MODPATH/$TARGET_OS_SINGLE_SYSTEM_IMAGE" ]; then
+        MODPATH="$MODPATH/$TARGET_OS_SINGLE_SYSTEM_IMAGE"
     fi
 
     if [ ! -f "$MODPATH/module.prop" ]; then

scripts/utils/common_utils.sh
@@ -123,7 +123,7 @@ _GET_SELINUX_LABEL()
             FC_FILE="$WORK_DIR/vendor/etc/selinux/vendor_file_contexts"
             ;;
         "system_ext")
-            if $TARGET_HAS_SYSTEM_EXT; then
+            if $TARGET_OS_BUILD_SYSTEM_EXT_PARTITION; then
                 FC_FILE="$WORK_DIR/system_ext/etc/selinux/system_ext_file_contexts"
             else
                 FC_FILE="$WORK_DIR/system/system/system_ext/etc/selinux/system_ext_file_contexts"
@@ -226,7 +226,7 @@ ADD_TO_WORK_DIR()
             SOURCE_FILE+="/system/system_ext/$FILE"
         fi
 
-        if $TARGET_HAS_SYSTEM_EXT; then
+        if $TARGET_OS_BUILD_SYSTEM_EXT_PARTITION; then
             TARGET_FILE+="/system_ext/$FILE"
         else
             PARTITION="system"
@@ -308,7 +308,7 @@ ADD_TO_WORK_DIR()
         FILES="$(find "${SOURCE_FILE%/.}")"
         FILES="${FILES//$SOURCE\//}"
         [[ "$PARTITION" == "system" ]] && FILES="${FILES//system\/system\//system/}"
-        $TARGET_HAS_SYSTEM_EXT || FILES="${FILES//system_ext\//system/system_ext/}"
+        $TARGET_OS_BUILD_SYSTEM_EXT_PARTITION || FILES="${FILES//system_ext\//system/system_ext/}"
 
         while IFS= read -r f; do
             IS_VALID_PARTITION_NAME "$f" && continue
@@ -404,7 +404,7 @@ DELETE_FROM_WORK_DIR()
         FILE="${FILE:1}"
     done
 
-    if ! $TARGET_HAS_SYSTEM_EXT && [[ "$PARTITION" == "system_ext" ]]; then
+    if ! $TARGET_OS_BUILD_SYSTEM_EXT_PARTITION && [[ "$PARTITION" == "system_ext" ]]; then
         PARTITION="system"
         FILE="system/system_ext/$FILE"
     fi
@@ -412,7 +412,7 @@ DELETE_FROM_WORK_DIR()
     local FILE_PATH="$WORK_DIR"
     case "$PARTITION" in
         "system_ext")
-            if $TARGET_HAS_SYSTEM_EXT; then
+            if $TARGET_OS_BUILD_SYSTEM_EXT_PARTITION; then
                 FILE_PATH+="/system_ext"
             else
                 FILE_PATH+="/system/system/system_ext"

scripts/utils/module_utils.sh
@@ -283,7 +283,7 @@ SET_PROP()
                 FILE="$WORK_DIR/system/system/build.prop"
                 ;;
             "system_ext")
-                if $TARGET_HAS_SYSTEM_EXT; then
+                if $TARGET_OS_BUILD_SYSTEM_EXT_PARTITION; then
                     FILE="$WORK_DIR/system_ext/etc/build.prop"
                 else
                     FILE="$WORK_DIR/system/system/system_ext/etc/build.prop"

commit 238cf666a2ee3f70e40c27ccffdbbc64cac852af

scripts: build_flashable_zip: fix regression caused by 016098bc
Change-Id: I700770e1c0b7c8d4b25eb487b6252340b4ddb465

scripts/internal/build_flashable_zip.sh
@@ -438,9 +438,11 @@ GET_SUPER_GROUP_SIZE()
     local GROUP_NAME="$TARGET_SUPER_GROUP_NAME"
     GROUP_NAME="$(tr "[:lower:]" "[:upper:]" <<< "$TARGET_SUPER_GROUP_NAME")"
 
-    _CHECK_NON_EMPTY_PARAM "TARGET_${GROUP_NAME}_SIZE" "${!TARGET_${GROUP_NAME}_SIZE}" || exit 1
+    local VAR="TARGET_${GROUP_NAME}_SIZE"
 
-    echo "${!TARGET_${GROUP_NAME}_SIZE}"
+    _CHECK_NON_EMPTY_PARAM "$VAR" "${!VAR}" || exit 1
+
+    echo "${!VAR}"
 }
 
 PRINT_HEADER()