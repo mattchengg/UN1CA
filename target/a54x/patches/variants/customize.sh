10e5d9fcb0e60522e647f992d9568414da0b0961
鄭秉正 <126307842+mattchengg@users.noreply.github.com>
2025-11-27 17:05:06 +0800

    Refactor SELinux policy rules addition

--- target/a54x/patches/variants/customize.sh
+++ target/a54x/patches/variants/customize.sh
@@ -1,15 +1,11 @@
-rm -rf "$WORK_DIR/vendor/tee"
+DELETE_FROM_WORK_DIR "vendor" "tee"
 mkdir -p "$WORK_DIR/vendor/tee"
 
-POLICY_FILE="$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil"
-
-read -r -d '' RULES <<'EOF'
-(allow init_33_0 tee_file (dir (mounton)))
-(allow priv_app_33_0 tee_file (dir (getattr)))
-(allow init_33_0 vendor_fw_file (file (mounton)))
-(allow priv_app_33_0 vendor_fw_file (file (getattr)))
-EOF
-
-if ! grep -q "(allow init_33_0 tee_file (dir (mounton)))" "$POLICY_FILE"; then
-    printf "%s\n" "$RULES" >> "$POLICY_FILE"
+if ! grep -q "init_33_0 tee_file" "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil"; then
+    {
+        echo "(allow init_33_0 tee_file (dir (mounton)))"
+        echo "(allow priv_app_33_0 tee_file (dir (getattr)))"
+        echo "(allow init_33_0 vendor_fw_file (file (mounton)))"
+        echo "(allow priv_app_33_0 vendor_fw_file (file (getattr)))"
+    } >> "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil"
 fi
